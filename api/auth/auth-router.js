b53914f0da74aa27e11ca3e1db26e81a0f337c76
Rob <rbhouck32@gmail.com>
2021-01-31 19:49:36 -0500

    testing for seed data

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -4,15 +4,18 @@ const bcrypt = require("bcryptjs");
 
 const Users = require("../users/users-model.js");
 const { validateUser, errorHandler } = require("./auth-services.js");
+// const { isValid } = require("../users/users-services.js");
 
 // [POST] = Registers a new user in the database
 router.post("/register", validateUser, async (req, res, next) => {
   const credentials = req.body;
+
   try {
     const rounds = process.env.BCRYPT_ROUNDS || 8;
     const hash = bcrypt.hashSync(credentials.password, rounds);
     credentials.password = hash;
     const newUser = await Users.add(credentials);
+
     res.status(201).json({ message: "Success!", newUser });
   } catch (error) {
     res.status(500).json(error);
--- api/countries/countries-router.js
+++ api/countries/countries-router.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
+const Countries = require("./countries-model.js");
 
-router.get("/", (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
+    const countries = await Countries.find();
+    res.status(200).json(countries);
+  } catch (err) {
+    next(err);
   }
 });
 
